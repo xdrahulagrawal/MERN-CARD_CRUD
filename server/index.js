64576c7c6eefc774a6d3e1962a4965493caf1b09
Rahul Agrawal <xdrahulagrawal@gmail.com>
2022-11-29 18:05:09 +0530

    created server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,17 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv'
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server start at port ${PORT}`);
+});
