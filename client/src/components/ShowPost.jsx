7ffdb6f86c23db9158bb6c403ee721403e53c7f1
Rahul Agrawal <xdrahulagrawal@gmail.com>
2022-12-01 15:03:27 +0530

    made card for ui

--- client/src/components/ShowPost.jsx
+++ client/src/components/ShowPost.jsx
@@ -4,12 +4,24 @@ import '../assests/styles/showpost.css'
 
 function ShowPost() {
   const { allPost } = useContext(PostContext);
-  console.log('posts', allPost)
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   return (
     <div className='show-post-container'>
       {allPost.map((post) => {
-        return <div key={post._id}>
-          <img src={post?.SelectedFile} width={300} height={300}/>
+        return <div key={post._id} className='show-post-container-item'>
+          <img src={post?.SelectedFile} width={300} height={200} />
+          <div className='show-post-sub-container-1'>
+            <p>{capitalizeFirstLetter(post?.title)}</p>
+            <p><b>Like :</b> {post?.likeCount}</p>
+          </div>
+          <div className='show-post-sub-container-2'>
+            <button>Delete</button>
+            <button>Edit</button>
+          </div>
         </div>
       })}
     </div>
