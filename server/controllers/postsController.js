881217af9be61c02abfdabdf850375b8eb8e9b39
Rahul Agrawal <xdrahulagrawal@gmail.com>
2022-12-01 14:30:45 +0530

    made update route

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import Posts from '../models/postsModel.js';
 
 export const getPosts = async (req, res) => {
@@ -21,3 +22,19 @@ export const createPost = async (req, res) => {
         res.status(409).json({ message: error.message });
     }
 }
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send({ message: 'No post with that id' })
+    }
+    try {
+        const updatePost = await Posts.findByIdAndUpdate(_id, post, { new: true })
+        res.json(updatePost)
+    } catch (error) {
+        console.error('Update Posts')
+        res.status(409).json({ message: error.message });
+    }
+}
+
